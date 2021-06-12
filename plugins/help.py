6b5fbd2087adf3963741a020ce7aea2b2a3ead08
makerwith <83757763+makerwith@users.noreply.github.com>
2021-06-12 08:54:09 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -38,9 +38,9 @@ def map(pos):
             [InlineKeyboardButton(text = '-->', callback_data = "help+2")]
         ]
     elif(pos==len(tr.HELP_MSG)-1):
-        url = "https://t.me/MaxxBots"
+        url = "https://t.me/close_chapters"
         button = [
-            [InlineKeyboardButton(text = 'Support Chat', url="https://t.me/MaxxBots")],
+            [InlineKeyboardButton(text = 'Support Chat', url="https://t.me/Movie_loverz_group")],
             [InlineKeyboardButton(text = 'Feature Request', url=url)],
             [InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}")]
         ]
